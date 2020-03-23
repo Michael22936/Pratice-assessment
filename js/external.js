9ba0478a78e81717283d1688b07ca51061b9aa16
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-03-23 13:15:31 -0500

    added a simple function in JS

--- js/external.js
+++ js/external.js
@@ -4,3 +4,9 @@ console.log('Hello JavaScript');
 alert("hello world");
 var confirmed = confirm("DO you want to do this");
 console.log(confirmed);
+
+function sayHello(x) {
+    alert(x)
+}
+
+sayHello("hello michael");
